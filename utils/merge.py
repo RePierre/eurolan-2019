d329e41b8af5f1d901d2d34924c7ed5527f04c7e
RePierre <petru.rebeja@gmail.com>
2019-05-06 17:22:53 +0300

    Added explicit arguments to merge script

--- utils/merge.py
+++ utils/merge.py
@@ -1,19 +1,37 @@
-# This script merges the manually annotated corpus with the GOLD corpus for training.
-# Author: Cristian Pădurariu
+# This script merges the manually annotated corpus with the rest of the corpus used for training.
+
 from bs4 import BeautifulSoup as bs
-import sys
+from argparse import ArgumentParser
 
-args = sys.argv
-filepath1 = args[1]
-filepath2 = args[2]
 
-f = open(filepath1, "r", encoding="utf-8")
-xml1 = bs(f.read(), 'xml')
-f = open(filepath2, "r", encoding="utf-8")
-xml2 = bs(f.read(), 'xml')
+def merge_corpora(args):
+    f = open(args.manually_annotated, "r", encoding="utf-8")
+    xml1 = bs(f.read(), 'xml')
+    f = open(args.training_corpus, "r", encoding="utf-8")
+    xml2 = bs(f.read(), 'xml')
 
-for sent in xml2.find_all("sentence"):
-    xml1.treebank.append(sent)
+    for sent in xml2.find_all("sentence"):
+        xml1.treebank.append(sent)
 
-f = open("train.xml", "w", encoding="utf-8")
-f.write(str(xml1))
+    f = open(args.output_corpus, "w", encoding="utf-8")
+    f.write(str(xml1))
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument('--manually-annotated',
+                        required=True,
+                        help='The manually annotated corpus.')
+    parser.add_argument('--training-corpus',
+                        required=True,
+                        help='The rest of the corpus used for training.')
+    parser.add_argument('--output-corpus',
+                        required=False,
+                        default='train.xml',
+                        help='The name of the merged corpus.')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    merge_corpora(args)
